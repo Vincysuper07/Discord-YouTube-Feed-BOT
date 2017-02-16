c7a7ef4f90c9c90d38a57343818ac276cb661895
anti0 <briu.briu55@gmail.com>
2017-02-16 20:11:43 +0100

    Add files via upload

--- main.py
+++ main.py
@@ -2,10 +2,11 @@ import discord
 import time
 import asyncio
 import sys
-from Implementation import Youtuber
+from Api_Test import Youtuber
 from config import Config
 
 config = Config('config.yml')
+client = discord.Client()
 
 youtubers = config.getYouTubersList() if (config.getYouTubersNr() != 0) else sys.exit()
 GOOGLE_API = config.getConnectionData()[0]
@@ -19,39 +20,31 @@ while i < config.getYouTubersNr():
     temp_list.append(config.getYouTubersList()[i]['name'])
     temp_list.append(config.getYouTubersList()[i]['id']) if not config.getYouTubersList()[i]['channelID'] else temp_list.append(config.getYouTubersList()[i]['channelID'])
     temp_list.append(True) if not config.getYouTubersList()[i]['id'] else temp_list.append(False)
+    temp_list.append('')
     threads.append(temp_list)
     i += 1
 
-print(threads)
-pewds = Youtuber(GOOGLE_API, threads[0][1], False)
-print(config.getYouTubersNr())
-
 i = 0
-while i < 3:
+while i < config.getYouTubersNr():
     processes.append(Youtuber(GOOGLE_API, threads[i][1], threads[i][2]))
     i += 1
-
-print(processes)
-#liveId = ''
-
-client = discord.Client()
 
 async def update():
     while True:
         item = 0
-        while item < 3:
+        while item < config.getYouTubersNr():
             data = processes[item].update()
             print('{} >> {}'.format(time.strftime("%H:%M:%S"), data))
             if processes[item].isNewVideo():
                 await client.send_message(client.get_channel('273549788283142166'), '{} just posted a new video!'.format(threads[item][0]))
                 await client.send_message(client.get_channel('273549788283142166'), '{}'.format(processes[item].getVideoLink(processes[item].videosData[0][1])))
+
+
+            if processes[item].isUserLive():
+                if not processes[item].liveId == threads[item][3]:
+                    await client.send_message(client.get_channel('273549788283142166'), '{} is live!'.format(threads[item][0]))
+                    await client.send_message(client.get_channel('273549788283142166'), '{}'.format(processes[item].getVideoLink(processes[item].getUserLiveData())))
             item += 1
-
-        #if api.isUserLive():
-        #    if not api.liveId == liveId:
-        #        await client.send_message(client.get_channel('273549788283142166'), '{} is live!'.format(YoutubeName))
-        #        await client.send_message(client.get_channel('273549788283142166'), '{}'.format(api.getVideoLink(api.getUserLiveData())))
-
         await asyncio.sleep(pingEveryXMinutes * 60)
 
 @client.event
